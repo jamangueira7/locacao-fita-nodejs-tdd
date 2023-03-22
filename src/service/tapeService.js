89da5624f20e624d8d09731faf98ecd60e6262c4
João Mangueira <jamjs7@gmail.com>
2023-03-22 11:20:49 -0300

    feat(tape): add test e primeira funcionalidade

--- src/service/tapeService.js
+++ src/service/tapeService.js
@@ -4,6 +4,11 @@ class TapeService {
     constructor({ repository }) {
         this.repository = repository;
     }
+
+    async getTapeById(id) {
+        const tape = await this.repository.find(id);
+        return tape;
+    }
 }
 
 module.exports = TapeService;
---
+++ test/unit/tapeService.test.js
@@ -0,0 +1,52 @@
+const { describe, it, before, beforeEach, afterEach } = require('mocha');
+const sinon = require('sinon');
+
+const { join } = require('path');
+const { expect } = require('chai');
+
+const TapeService = require('./../../src/service/tapeService');
+const TapeRepository = require('./../../src/repository/tapeRepository');
+
+const tapeDatabase  = join(__dirname, './../../database', "categories.json");
+
+const mocks = {
+    validTape: require('./../mocks/tape/valid-tape.json'),
+};
+
+
+tapeRepository = new TapeRepository({
+    file: tapeDatabase
+});
+
+const mockRepositoryGetTapeById = sinon.stub(tapeRepository, 'find');
+mockRepositoryGetTapeById.resolves(mocks.validTape);
+
+describe('TapeService Suite Tests', () => {
+
+    let tapeService = {};
+    let sandbox = {};
+
+    before(() => {
+        tapeService = new TapeService({
+            repository: tapeRepository
+        });
+    });
+
+    beforeEach(() => {
+        sandbox = sinon.createSandbox();
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    it('should return tape by id', async () => {
+        const id = "8186123a-aaa3-414e-a1f6-8888e2cee196";
+        const expected = mocks.validTape;
+
+        const result = await tapeService.getTapeById(id);
+
+        expect(result).to.eql(expected);
+
+    });
+});
